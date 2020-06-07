95ee43d85e50754176686ca8b3123a63a12488e4
Alvaro Flaño <tikoflano@gmail.com>
2020-06-07 16:24:04 -0400

    Mejoras al script. Ahora se puede ejecutar en modo normal ("server") o forzar modo "diagnostic"

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ Instalar Docker y Docker Compose
 ### Procedimiento
 Copiar el contenido de la carpeta de la base de datos del cliente en la carpeta **database** y luego ejecutar los comandos del listado:
 
-* docker-compose up -d --force-recreate
-* docker cp database mysql:/bitnami/mysql/data
-* docker-compose exec --user=root mysql /recover.sh
-* docker cp mysql:/recover.sql ./recover.sql
+* `docker-compose up -d --force-recreate`
+* `docker cp database mysql:/bitnami/mysql/data`
+* `docker-compose exec --user=root mysql /recover.sh` o `docker-compose exec --user=root mysql /recover.sh --diagnostic`
+* `docker cp mysql:/recover.sql ./recover.sql`
 
 Esto generará el archivo **recover.sql** el cual es un dump de la base de datos.
--- recover.sh
+++ recover.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
+
+# Install mysql-utilities
 curl -O http://ftp.cl.debian.org/debian/pool/main/m/mysql-utilities/mysql-utilities_1.6.4-1_all.deb
 apt-get update && apt install -y ./mysql-utilities_1.6.4-1_all.deb
-cd /bitnami/mysql/data
-chown -R 1001 database/
-cd database/
-echo 'CREATE DATABASE recover;' | mysql
+
+# Fix permissions
+chown -R 1001 /bitnami/mysql/data/database
+
+# Run mysqlfrm
+cd /bitnami/mysql/data/database
 FRM_FILES="";for file in *.ibd;do FRM_FILES="$FRM_FILES ${file%.*}.frm";done
-{ mysqlfrm --server=root2@localhost --port=3307 --user=root $FRM_FILES 2> /dev/null || mysqlfrm --diagnostic $FRM_FILES; } | grep -v "#" | awk 'BEGIN {RS = "\n\n"} {print $0";"}' > /tmp/dump.sql
+if [ "$1" == "--diagnostic" ]; then
+  mysqlfrm --diagnostic $FRM_FILES
+else
+  mysqlfrm --server=root@localhost --port=3307 --user=root $FRM_FILES
+fi | grep -v "#" | awk 'BEGIN {RS = "\n\n"} {print $0";"}' > /tmp/dump.sql
+
+# Create database and tables
+echo 'CREATE DATABASE recover;' | mysql
 mysql recover < /tmp/dump.sql
+
+# Recover tables data
 echo 'USE INFORMATION_SCHEMA; SELECT CONCAT("ALTER TABLE `", TABLE_SCHEMA,"`.`", TABLE_NAME, "` DISCARD TABLESPACE;") AS MySQLCMD FROM TABLES WHERE TABLE_SCHEMA = "recover" AND ENGINE = "InnoDB";' | mysql -N | xargs | mysql
 /bin/cp -a ./* ../recover/
 echo 'USE INFORMATION_SCHEMA; SELECT CONCAT("ALTER TABLE `", TABLE_SCHEMA,"`.`", TABLE_NAME, "` IMPORT TABLESPACE;") AS MySQLCMD FROM TABLES WHERE TABLE_SCHEMA = "recover" AND ENGINE = "InnoDB";' | mysql -N | xargs | mysql
+
+# Create sqldump
 mysqldump recover > /recover.sql
-exit
